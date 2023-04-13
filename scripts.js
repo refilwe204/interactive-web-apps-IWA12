bbf30e85c5b6b3a7490db56d7d07e17a2aee77cc
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-13 14:53:06 +0200

    final commit to IWA12

--- scripts.js
+++ scripts.js
@@ -1,54 +1,56 @@
+// scripts.js
+
 const STATUS_MAP = {
     shelf: {
-        color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
+      color: 'green',
+      canReserve: true,
+      canCheckout: true,
+      canCheckIn: false,
     },
     reserved: {
-        color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
+      color: 'blue',
+      canReserve: false,
+      canCheckout: true,
+      canCheckIn: false,
     },
     overdue: {
-        color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+      color: 'red',
+      canReserve: false,
+      canCheckout: false,
+      canCheckIn: true,
     },
     checkedOut: {
-        color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+      color: 'orange',
+      canReserve: false,
+      canCheckout: false,
+      canCheckIn: true,
     }
-}
-
-// Get all relevant elements
-const statusElements = document.querySelectorAll('.status');
-const reserveButtons = document.querySelectorAll('.reserve');
-const checkoutButtons = document.querySelectorAll('.checkout');
-const checkinButtons = document.querySelectorAll('.checkin');
-
-// Update each book element based on its status
-for (let i = 0; i < statusElements.length; i++) {
-  const statusText = statusElements[i].textContent.trim().toLowerCase();
-  const status = STATUS_MAP[statusText];
-
-  // Update status text color
-  statusElements[i].style.color = status.color;
-
-  // Enable/disable buttons
-  reserveButtons[i].disabled = !status.canReserve;
-  checkoutButtons[i].disabled = !status.canCheckout;
-  checkinButtons[i].disabled = !status.canCheckIn;
-
-  // Set button color to black and grayscale
-  reserveButtons[i].style.color = 'black';
-  checkoutButtons[i].style.color = 'black';
-  checkinButtons[i].style.color = 'black';
-  reserveButtons[i].style.filter = 'grayscale(100%)';
-  checkoutButtons[i].style.filter = 'grayscale(100%)';
-  checkinButtons[i].style.filter = 'grayscale(100%)';
-}
+  };
+  
+  // Select all the elements
+  const statusElements = document.querySelectorAll('.status');
+  const reserveButtons = document.querySelectorAll('.reserve');
+  const checkoutButtons = document.querySelectorAll('.checkout');
+  const checkinButtons = document.querySelectorAll('.checkin');
+  
+  // Loop through all the book elements
+  for (let i = 0; i < statusElements.length; i++) {
+    const status = statusElements[i].textContent.trim();
+    const statusInfo = STATUS_MAP[status];
+  
+    // Set the color of the status element
+    statusElements[i].style.color = statusInfo.color;
+  
+    // Set the status of the reserve button
+    reserveButtons[i].disabled = !statusInfo.canReserve;
+    reserveButtons[i].style.color = reserveButtons[i].disabled ? 'gray' : 'black';
+  
+    // Set the status of the checkout button
+    checkoutButtons[i].disabled = !statusInfo.canCheckout;
+    checkoutButtons[i].style.color = checkoutButtons[i].disabled ? 'gray' : 'black';
+  
+    // Set the status of the checkin button
+    checkinButtons[i].disabled = !statusInfo.canCheckIn;
+    checkinButtons[i].style.color = checkinButtons[i].disabled ? 'gray' : 'black';
+  }
+  
